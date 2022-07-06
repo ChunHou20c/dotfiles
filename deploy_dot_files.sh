5ecb48d120ec02bef9fef5628d7c21e6db1c6dfa
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-07-06 12:24:08 +0800

    added i3 config and deployment commands

--- deploy_dot_files.sh
+++ deploy_dot_files.sh
@@ -4,3 +4,6 @@ CURRENT_DIRECTORY=`pwd`
 
 ln -s $CURRENT_DIRECTORY"/.vimrc" $HOME/.vimrc
 ln -s $CURRENT_DIRECTORY"/kitty.conf" $HOME/.config/kitty/kitty.conf
+
+ln -s $CURRENT_DIRECTORY"/config" $HOME/.config/i3/config
+ln -s $CURRENT_DIRECTORY"/i3.conf.d" $HOME/.config/i3/i3.conf.d
